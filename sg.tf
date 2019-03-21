685531a7ae07765ab3e0303668bddeee30a920e2
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-21 14:49:15 +0000

    better naming

--- sg.tf
+++ sg.tf
@@ -3,8 +3,8 @@
 #
 resource "aws_security_group" "ec2_sg" {
   count       = "${var.count}"
-  name        = "Security group for controlling access to ${var.hostname_prefix}${format("%03d", count.index + 1 + var.hostname_offset)}.${var.domain_name}"
-  description = "Security group for controlling access to ${var.hostname_prefix}${format("%03d", count.index + 1 + var.hostname_offset)}.${var.domain_name}"
+  name        = "SG.${var.hostname_prefix}${format("%03d", count.index + 1 + var.hostname_offset)}.${var.domain_name}"
+  description = "SG.${var.hostname_prefix}${format("%03d", count.index + 1 + var.hostname_offset)}.${var.domain_name}"
   vpc_id      = "${var.vpc_id}"
 
   # Allow all outbound traffic
