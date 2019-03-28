61fd6858398b54d474d8158cfc41eaee3e6e4de0
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-28 15:50:03 +0000

    count on sg rukes

--- sg_rules.tf
+++ sg_rules.tf
@@ -9,17 +9,22 @@ resource "aws_security_group_rule" "sshaccess" {
   to_port                  = "22"
   protocol                 = "TCP"
   source_security_group_id = "${element(split(",",var.ssh_access_sgs),count.index)}"
-  security_group_id        = "${aws_security_group.ec2_ssh_sg.id}"
+  security_group_id        = "${element(split(",",aws_security_group.ec2_ssh_sg.id),count.index)" 
+
+#"${aws_security_group.ec2_ssh_sg.id}"
 }
 
 #
 # Allow ec2 SELF
 #
 resource "aws_security_group_rule" "ec2_self_ingress" {
+  count                    = "${var.count}"
   type                     = "ingress"
   from_port                = 0
   to_port                  = 65535
   protocol                 = "-1"
-  source_security_group_id = "${aws_security_group.ec2_sg.id}"
-  security_group_id        = "${aws_security_group.ec2_sg.id}"
+  #source_security_group_id = "${aws_security_group.ec2_sg.id}"
+  #security_group_id        = "${aws_security_group.ec2_sg.id}"
+  source_security_group_id = "${element(split(",",aws_security_group.ec2_sg.id,count.index)"
+  security_group_id        = "${element(split(",",aws_security_group.ec2_sg.id,count.index)"
 }
